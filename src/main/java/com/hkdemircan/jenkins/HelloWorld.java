860eead799eee9c003358c4161dfeb6d51afa140
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2019-10-11 22:57:04 +0300

    init

---
+++ src/main/java/com/hkdemircan/jenkins/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.hkdemircan.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorld {
+	  @GetMapping
+	    public String getTodos(){
+	        return "hello jenkins test";
+	    }
+	
+}
