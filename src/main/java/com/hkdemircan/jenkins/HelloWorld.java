4726eda65a013e87ad7cb0eefb02790f69e348dd
Hasan Kadir Demircan <kadir.demircan@linktera.com.tr>
2019-10-15 17:53:20 +0300

    test

--- src/main/java/com/hkdemircan/jenkins/HelloWorld.java
+++ src/main/java/com/hkdemircan/jenkins/HelloWorld.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 	  @GetMapping
 	    public String getTodos(){
-	        return "hello jenkins test3";
+	        return "hello jenkins test4";
 	    }
 	
 }
